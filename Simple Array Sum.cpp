6f9bcd362bff3aedb13d6e112c022c94e483fabb
Upendra Sharma <upendra861@gmail.com>
2018-03-06 00:58:03 +0530

    Simple Array Sum
    Given an array of integers, find the sum of its elements.
    Input Format
    The first line contains an integer, , denoting the size of the array. 
    The second line contains  space-separated integers representing the array's elements.
    Output Format
    Print the sum of the array's elements as a single integer.
    Sample Input
    6
    1 2 3 4 10 11
    Sample Output
    31

---
+++ Simple Array Sum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int simpleArraySum(int n, vector <int> ar) 
+{
+    int c=0;
+    for(int ar_i = 0; ar_i < n; ar_i++){
+       c+=ar[ar_i];
+    }
+    return c;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> ar(n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+       cin >> ar[ar_i];
+    }
+    int result = simpleArraySum(n, ar);
+    cout << result << endl;
+    return 0;
+}
