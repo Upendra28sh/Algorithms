f197e2f8af7d28826119b262d2f110bfc7bc1f85
Upendra Sharma <upendra861@gmail.com>
2018-03-25 20:03:36 +0530

    Drawing Book
    Brie’s Drawing teacher asks her class to open their n-page book to page number p . Brie can either start turning pages from the front of the book (i.e., page number 1) or from the back of the book (i.e., page number n), and she always turns pages one-by-one (as opposed to skipping through multiple pages at once). When she opens the book, page 1 is always on the right side:
    Each page in the book has two sides, front and back, except for the last page which may only have a front side depending on the total number of pages of the book (see the Explanation sections below for additional diagrams).
    Given n and p, find and print the minimum number of pages Brie must turn in order to arrive at page p.
    Input Format
    The first line contains an integer, n, denoting the number of pages in the book. 
    The second line contains an integer, p, denoting the page that Brie's teacher wants her to turn to.
    Output Format
    Print an integer denoting the minimum number of pages Brie must turn to get to page p.
    Sample Input 0
    6
    2
    Sample Output 0
    1Sample Input 1
    5
    4
    Sample Output 1
    0

---
+++ Drawing Book .cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int n, int p)
+{    
+       int front=0;
+       int back=0;
+       front=(p-0)/2;
+       back=(n-p)/2;
+       if(front<=back)
+       {
+           return front;
+       }
+       else
+       {
+           if(n%2==0&&p%2!=0)
+           {
+               return back+1;
+           }
+           else
+           return back;    
+       } 
+}
+
+int main() 
+{
+    int n;
+    cin >> n;
+    int p;
+    cin >> p;
+    int result = solve(n, p);
+    cout << result << endl;
+    return 0;
+}
