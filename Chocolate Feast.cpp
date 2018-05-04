25ba0391616106c4c4cb52c6063a878e00491840
Upendra Sharma <upendra861@gmail.com>
2018-05-04 23:41:50 +0530

    Chocolate Feast
    Little Bobby loves chocolate. He frequently goes to his favorite  store, Penny Auntie, to buy them. They are having a promotion at Penny Auntie. If Bobby saves enough wrappers, he can turn them in for a free chocolate.
    
    Complete the chocolateFeast function in the code stub below to return the number of chocolates Bobby can eat with a given amount of money after taking full advantage of the promotion.
    
    Note: Little Bobby will always turn in his wrappers if he has enough to get a free chocolate.
    
    Input Format
    
    The first line contains an integer, t, denoting the number of scenarios to analyze. 
    Each of the next t lines contains three space-separated integers: n, c, and m. They represent money to spend, cost of a chocolate, and the number of wrappers he can turn in for a free chocolate.
    
    Output Format
    
    For each trip to Penny Auntie, print the total number of chocolates Bobby eats on a new line.
    
    Sample Input
    
    3
    10 2 5
    12 4 4
    6 2 2
    Sample Output
    
    6
    3
    5

---
+++ Chocolate Feast.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int chocolateFeast(int n, int c, int m) 
+{
+    int iw=0;
+    iw=n/c;
+    int count=iw;
+    int nw=0;
+    int r=0;
+    int a=iw;
+    
+    while(a>=m)
+    {
+    nw=a/m;
+    r=iw%m;
+    count+=nw;
+    a=r+nw;
+    }
+        
+    return count;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        int n;
+        int c;
+        int m;
+        cin >> n >> c >> m;
+        int result = chocolateFeast(n, c, m);
+        cout << result << endl;
+    }
+    return 0;
+}
